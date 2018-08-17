6c5cf0065c5a36a3a32ba62aaa6d6f16d1aec37c
Lars Rosenquist <lrosenquist@pivotal.io>
2018-08-17 13:32:29 +0200

    Added service registry

--- src/main/java/io/pivotal/cf/workshop/WorkshopApplication.java
+++ src/main/java/io/pivotal/cf/workshop/WorkshopApplication.java
@@ -4,10 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @Slf4j
+@EnableDiscoveryClient
 public class WorkshopApplication {
 
 	public static void main(String[] args) {
