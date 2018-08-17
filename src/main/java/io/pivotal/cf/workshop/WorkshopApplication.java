63425bbc96e64337633a611b1d50673721ebaa64
Lars Rosenquist <lrosenquist@pivotal.io>
2018-08-17 09:29:47 +0200

    Initial version.

---
+++ src/main/java/io/pivotal/cf/workshop/WorkshopApplication.java
@@ -0,0 +1,12 @@
+package io.pivotal.cf.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopApplication.class, args);
+	}
+}
